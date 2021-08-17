51d8b9c627f58e838d75dbe603b93aa4124ab8e7
Shubham Bhardwaj <shubham.0638@gmail.com>
2021-08-17 15:20:21 +0530

    Staging (#1)
    * Staging check
    
    * Checking for policy page
    
    * Policy page added. Working now.

--- next.config.js
+++ next.config.js
@@ -3,11 +3,9 @@ const optimizedImages = require('next-optimized-images');
 
 const nextConfiguration = {
   target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
-  
-  rewrites: async () => [
-    {source: '/privacy', destination: '/api/privacy'}
+  redirects: async () => [
+    {source: '/api/privacy', destination: '/privacyPolicy', permanent: true}
   ]
-
 };
 
 module.exports = withPlugins([optimizedImages], nextConfiguration);
